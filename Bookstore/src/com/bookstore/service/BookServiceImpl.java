11e3098b4a213574b9b755f757778079bc30cdac
alokAHamana <165271458+alokAHamana@users.noreply.github.com>
2024-03-30 21:25:24 +0530

    Update BookServiceImpl.java  by alok

--- Bookstore/src/com/bookstore/service/BookServiceImpl.java
+++ Bookstore/src/com/bookstore/service/BookServiceImpl.java
@@ -1,6 +1,32 @@
 package com.bookstore.service;
 
 public class BookServiceImpl implements BookService {
+
+
+	// declare instance variable
+	String jdbcURL ;
+	String jdbcUsername ;
+	String jdbcPassword ;
+	
+	// intialize valuee through Constructor
+
+	public BookServiceImpl(String jdbcURL , String jdbcUsername, String jdbcPassword) {
+		
+		this.jdbcURL =jdbcURL;
+		this.jdbcUsername = jdbcUsername;
+		this.jdbcPassword = jdbcPassword;
+	}
+	private BookDao  bookDao = new BookDAOImpl();
+
+	//------ato
+	
+	@Override
+	public void insertBook(Book book) throws SQLException {
+		
+		System.out.println("BookServiceImpl :"+book.getTitle() +" "+ book.getAuthor()+ " "+ book.getPrice());
+
+		bookDao.insertBook(book);
+	}	
 	
 
 }
